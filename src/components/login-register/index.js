f2390c6056b69e0be32511ebb1095d0635263ee3
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-03-17 13:46:27 +0300

    registration UI part and header UI part completed

--- src/components/login-register/index.js
+++ src/components/login-register/index.js
@@ -1,19 +1,8 @@
-import React, { useState } from "react";
 import Register from "./Register";
 import Login from "./Login";
-import Header from "./../../components/header/index";
 
-const SignInSignUp = () => {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  function submitForm() {
-    setIsSubmitted(true);
-  }
-  return (
-    <div>
-      {!isSubmitted ? <Register submitForm={submitForm} /> : <Header />}
-    </div>
-  );
+const SignInSignUp = ({ submitForm, isSubmitted }) => {
+  return <div>{!isSubmitted && <Register submitForm={submitForm} />}</div>;
 };
 
 export default SignInSignUp;
