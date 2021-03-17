a01be98976f774b07025cdc3a29fdf37a71e3ff1
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-03-17 13:10:15 +0300

    header options completed

--- src/components/header/HeaderOption.js
+++ src/components/header/HeaderOption.js
@@ -1,13 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styles from "./style.module.css";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Avatar from "./Avatar";
 
-function HeaderOption({ icon, title }) {
+function HeaderOption({ avatar, icon, title }) {
   return (
     <div className={styles.headerOptions}>
-      {title}
-      {icon && <FontAwesomeIcon icon={icon} />}
+      {icon && <FontAwesomeIcon icon={icon} className={styles.headerIcons} />}
+      {avatar && <Avatar src={avatar} className={styles.headerIcons} />}
+      <span>{title}</span>
     </div>
   );
 }
--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,13 +1,24 @@
 import React from "react";
 import styles from "./style.module.css";
 import HeaderOption from "./HeaderOption";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import avatar from "./../../assets/images/avatar.png";
+import {
+  faHome,
+  faUserFriends,
+  faSuitcase,
+  faCommentDots,
+  faBell,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
   return (
-    <div>
-      <HeaderOption title="Home" icon={faSearch} />
+    <div className={styles.navigation}>
+      <HeaderOption title="Home" icon={faHome} />
+      <HeaderOption title="Network" icon={faUserFriends} />
+      <HeaderOption title="Jobs" icon={faSuitcase} />
+      <HeaderOption title="Messaging" icon={faCommentDots} />
+      <HeaderOption title="Notifications" icon={faBell} />
+      <HeaderOption title="Me" avatar={avatar} />
     </div>
   );
 }
